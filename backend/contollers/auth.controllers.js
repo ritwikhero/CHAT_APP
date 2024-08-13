281bf4018e2c68ba152e09dbc43b188da685a1d0
Ritwik <ritwikhero@gmail.com>
2024-08-13 13:16:41 +0530

    Logout route added and tested on postman

--- backend/contollers/auth.controllers.js
+++ backend/contollers/auth.controllers.js
@@ -79,5 +79,12 @@ export const login = async (req,res) =>{
 }
 
 export const logout = (req,res) =>{
-    console.log("logoutUser");
+    // console.log("logoutUser");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message : "User logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error : "Internal server error"});
+    }
 }
