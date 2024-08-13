53043c576a82db352301381a37902a3d8653aaf7
Ritwik <ritwikhero@gmail.com>
2024-08-13 12:55:27 +0530

    Login route created and tested on postman

--- backend/contollers/auth.controllers.js
+++ backend/contollers/auth.controllers.js
@@ -53,8 +53,29 @@ export const signup = async(req,res) =>{
     // console.log("signupUser");
 }
 
-export const login = (req,res) =>{
-    console.log("loginUser");
+export const login = async (req,res) =>{
+    // console.log("loginUser");
+    try {
+        const {userName,Password} = req.body;
+        const user = await User.findOne({userName});
+        const isPasswordCorrect = await bcrypt.compare(Password,user?.Password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error : "Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            userName : user.userName,
+            profilePic : user.profilePic
+        });
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        return res.status(500).json({error : "Internal server error"});
+    }
 }
 
 export const logout = (req,res) =>{
