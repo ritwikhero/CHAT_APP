d0d682a25456bcd349fe63066a1aa6639fd3140b
Ritwik <ritwikhero@gmail.com>
2024-08-16 14:20:15 +0530

    sendMessage API added with protected route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
 import connectToMongoDb from "./db/dbConnection.js";
 
 dotenv.config();
@@ -8,8 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // extract incoming json data
-
+app.use(cookieParser()); // extracts cookie from the header
 app.use("/api/auth",authRoutes);
+app.use("/api/messages", messageRoutes)
+
 // app.get("/",(req,res)=>{
 //     //root route http://localhost:8000/
 //     res.send("Hello World");
