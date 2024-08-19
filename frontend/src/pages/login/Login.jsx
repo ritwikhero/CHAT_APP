6251665dfa7dd92e3adfe25b151dd17e21fc5448
Ritwik <ritwikhero@gmail.com>
2024-08-19 17:30:48 +0530

    Fronted UI work started

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify min-w-96 mx-auto'> 
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdriop-filter backdrop-blur-lg bg-opacity-0'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                Login 
+                <span className='text-blue-500'> Chat App </span>
+            </h1>
+            <form>
+                
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
