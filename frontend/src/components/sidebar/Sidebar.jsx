2cee973bd39f244143cc571fbf58b1b825c2bcee
Ritwik <ritwikhero@gmail.com>
2024-08-21 21:46:41 +0530

    home UI continued

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SearchInput from './SearchInput'
+import Conversations from './Conversations'
 const Sidebar = () => {
   return (
     <div>
         <SearchInput />
+        <Conversations />
         <div className='divider px-3'></div>
-        {/* <Conversation />
-        <Logout /> */}
+         
+        {/* <Logout /> */} 
     </div>
   )
 }
