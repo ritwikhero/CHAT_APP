ecfe83056e38134c4e418e109f9ba17102b352d9
Ritwik <ritwikhero@gmail.com>
2024-09-01 13:47:20 +0530

    useContext added and logout facility added

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -1,9 +1,11 @@
 import  {useState}  from 'react';
 import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
 
 
 const useSignUp = () => {
     const [loading,setLoading] = useState(false);
+    const{authUser,setAuthUser} = useAuthContext();
 
     const signup = async({fullName,userName,Password,confirmPassword,gender}) => {
         const success = handleInputErrors({fullName,userName,Password,confirmPassword,gender});
@@ -24,6 +26,13 @@ const useSignUp = () => {
             });
 
             const data = await res.json();
+            if(data.error){
+                throw new Error(data.error);
+            }
+            // localStorage
+            localStorage.setItem("chat-user",JSON.stringify(data));
+            // context
+            setAuthUser(data);
             console.log(data);
         } 
         
